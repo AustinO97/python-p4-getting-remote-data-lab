ebdb2411f5ce41cb51fbe3f260ff4595eb0c03de
austin <austinoblow97@gmail.com>
2024-05-13 10:46:31 -0400

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,31 +6,31 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        search_term = 'Daniel'
+    # def get_response_body(self):
+    #     search_term = 'Daniel'
 
-        search_term_formatted = search_term.replace(' ', '+')
-        fields = ['name', 'occupation']
-        fields_formatted = ','.join(fields)
-        limit = 1
+    #     search_term_formatted = search_term.replace(' ', '+')
+    #     fields = ['name', 'occupation']
+    #     fields_formatted = ','.join(fields)
+    #     limit = 1
 
-        URL = f'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json?name={search_term_formatted}&fields={fields_formatted}&limit={limit}'
+    #     URL = f'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json?name={search_term_formatted}&fields={fields_formatted}&limit={limit}'
 
-        response = requests.get(URL)
-        return response.content
+    #     response = requests.get(URL)
+    #     return response.content
 
-    def load_json(self):
-        search_term = 'Daniel'
+    # def load_json(self):
+    #     search_term = 'Daniel'
 
-        search_term_formatted = search_term.replace(' ', '+')
-        fields = ['name', 'occupation']
-        fields_formatted = ','.join(fields)
-        limit = 1
+    #     search_term_formatted = search_term.replace(' ', '+')
+    #     fields = ['name', 'occupation']
+    #     fields_formatted = ','.join(fields)
+    #     limit = 1
 
-        URL = f'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json?name={search_term_formatted}&fields={fields_formatted}&limit={limit}'
+    #     URL = f'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json?name={search_term_formatted}&fields={fields_formatted}&limit={limit}'
 
-        response = requests.get(URL).json()      
-        return response
+    #     response = requests.get(URL).json()      
+    #     return response
     
     def get_response_body(self):
         response = requests.get(self.url)
